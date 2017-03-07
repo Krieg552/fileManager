9cf926053ad8b06a8eb9670bf7899851b3b4fb6d
Krieg552 <primaryqa1@gmail.com>
2017-03-07 14:34:13 +0200

    Update Main.java

--- rdr/src/com/company/Main.java
+++ rdr/src/com/company/Main.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException{
         Scanner newScanner= new Scanner(System.in);
-
-        System.out.print("Enter folder Path: ");
-        String folderPath = newScanner.next();
-
-        System.out.print("Enter folder Name: ");
-        String folderName = newScanner.next();
-
         fileManager newFileManager=new fileManager();
 
+        System.out.print("Create File, please Enter filepath : ");
+        String filePath = newScanner.next();
+        System.out.print("Enter fileName to create: ");
+        String fileName = newScanner.next();
+        newFileManager.createNewFile(filePath, fileName);
+
+        System.out.print("Delete file, please enter filepath: ");
+        String rmFilePath = newScanner.next();
+        System.out.print("Enter fileName to delete: ");
+        String rmFileName = newScanner.next();
+        newFileManager.deleteSomeFile(rmFilePath, rmFileName);
+
+        System.out.print("Enter new folder Path to create: ");
+        String folderPath = newScanner.next();
+        System.out.print("Enter new folder Name to create: ");
+        String folderName = newScanner.next();
         newFileManager.createDirectory(folderPath, folderName);
 
-        System.out.print("Enter filepath: ");
-        String filePath = newScanner.next();
-
-        System.out.print("Enter fileName: ");
-        String fileName = newScanner.next();
-
-        newFileManager.createNewFile(filePath, fileName);
     }
 }
