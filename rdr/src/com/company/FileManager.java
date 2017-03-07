28ac4a16fc8714b9a1f52e7fea8b48dd742cf34a
Krieg552 <primaryqa1@gmail.com>
2017-03-07 14:34:00 +0200

    Update FileManager.java

--- rdr/src/com/company/FileManager.java
+++ rdr/src/com/company/FileManager.java
@@ -4,12 +4,30 @@ import java.io.IOException;
 
 public class fileManager {
 
-    public void createDirectory(String folderPath, String folderName) {
-        File dir1 = new File(folderPath + folderName);
-        dir1.mkdir();
-    }
+
     public void createNewFile(String path, String fileName) throws IOException {
-        File file1 = new File(path + fileName);
-        file1.createNewFile();
+        File newFile = new File(path + fileName);
+        if (newFile.createNewFile()) {
+            System.out.println("file was successfully created");
+            System.out.println(newFile.getAbsolutePath());
+        } else
+            System.out.println("file was not created");
+    }
+
+    public void deleteSomeFile(String path, String fileName) throws IOException {
+        File rmFile = new File(path + fileName);
+        if (rmFile.delete()) {
+            System.out.println("file was successfully deleted");
+        } else
+            System.out.println("file was not deleted");
+    }
+
+    public void createDirectory(String folderPath, String folderName) {
+        File directory = new File(folderPath + folderName);
+        if (directory.mkdir()) {
+            System.out.println("directory was successfully created");
+            System.out.println(directory.getAbsolutePath());
+        } else
+            System.out.println("directory was not created");
     }
 }
